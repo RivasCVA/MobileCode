44567b5036812a4060d22b4cba4061a1a35c261f
Carlos Rivas <rivascarlosva@gmail.com>
2022-01-07 22:40:42 -0600

    Add unit tests for problems GET request

--- server/src/routes/__tests__/problems.test.ts
+++ server/src/routes/__tests__/problems.test.ts
@@ -5,31 +5,32 @@ import env from '@root/util/env';
 
 const URL = '/api/problems';
 
+const DATABASE_URL = `mongodb://${env.HOST}:${env.DB_PORT}/${env.DB_NAME}`;
+
 describe(`GET ${URL}`, () => {
     beforeAll(async () => {
-        const url = `mongodb://${env.HOST}:${env.DB_PORT}/${env.DB_NAME}`;
-        await mongoose.connect(url);
+        await mongoose.connect(DATABASE_URL);
     });
 
     afterAll(async () => {
         await mongoose.connection.close();
     });
 
-    it('OK status code', async () => {
+    it('OK status code on regular request', async () => {
         // Arrange
 
         // Act
-        const response = await request(app).get(URL).send();
+        const response = await request(app).get(URL);
 
         // Assert
         expect(response.statusCode).toBe(200);
     });
 
-    it('Responds with all problems', async () => {
+    it('responds with all problems', async () => {
         // Arrange
 
         // Act
-        const response = await request(app).get(URL).send();
+        const response = await request(app).get(URL);
         const { body } = response;
         const problem = body[0];
         const { name, directory, description, difficulty, category } = problem;
@@ -45,5 +46,78 @@ describe(`GET ${URL}`, () => {
         expect(description).toBeTruthy();
         expect(difficulty).toBeTruthy();
         expect(category).toBeTruthy();
+    });
+});
+
+describe(`POST ${URL}`, () => {
+    beforeAll(async () => {
+        await mongoose.connect(DATABASE_URL);
+    });
+
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
+
+    it('SUCCESS status code on correct request', async () => {
+        // Arrange
+        const data = {
+            name: 'Test Name',
+            directory: 'test-directory',
+            description: 'Test description',
+            difficulty: 'easy',
+            category: 'arrays',
+        };
+
+        // Act
+        const response = await request(app).post(URL).send(data);
+
+        // Assert
+        expect(response.statusCode).toBe(201);
+    });
+
+    it('BAD_REQUEST status code on incorrect request', async () => {
+        // Arrange
+        const data = {
+            name: 'Test Name',
+            directory: 'test-directory',
+            description: 'Test description',
+            difficulty: 'easy',
+            category: '',
+        };
+
+        // Act
+        const response = await request(app).post(URL).send(data);
+
+        // Assert
+        expect(response.statusCode).toBe(400);
+    });
+
+    it('successfully adds new problem', async () => {
+        // Arrange
+        const testName = 'Test Name';
+        const testDirectory = 'test-directory';
+        const testDescription = 'Test description';
+        const testDifficulty = 'easy';
+        const testCategory = 'arrays';
+
+        const data = {
+            name: testName,
+            directory: testDirectory,
+            description: testDescription,
+            difficulty: testDifficulty,
+            category: testCategory,
+        };
+
+        // Act
+        const response = await request(app).post(URL).send(data);
+        const { body } = response;
+        const { name, directory, description, difficulty, category } = body;
+
+        // Assert
+        expect(name).toBe(testName);
+        expect(directory).toBe(testDirectory);
+        expect(description).toBe(testDescription);
+        expect(difficulty).toBe(testDifficulty);
+        expect(category).toBe(testCategory);
     });
 });
