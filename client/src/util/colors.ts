eda984e3626da19c073c66671bd283111c866348
Carlos Rivas <rivascarlosva@gmail.com>
2022-01-10 22:31:13 -0600

    Add color palette

---
+++ client/src/util/colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+    PrimaryBackground: '#FAFAFA',
+    SecondaryBackground: '#F0F0F0',
+    PrimaryDarkText: '#323232',
+    SecondaryDarkText: '#626783',
+    PrimaryLightText: '#FAFAFA',
+    SecondaryLightText: '#F6F5F8',
+    White: '#FAFAFA',
+    Black: '#323232',
+    DarkGray: '#626783',
+    LightGray: '#B0B3C1',
+    Purple: '#1F3078',
+    Green: '#46C08E',
+    Blue: '#009BDE',
+    Red: '#D23F00',
+};
+
+export default Colors;
