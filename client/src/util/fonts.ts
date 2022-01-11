749cb53112cbe176d062c1dcbbdc12d019a8ca5f
Carlos Rivas <rivascarlosva@gmail.com>
2022-01-10 22:25:16 -0600

    Add Poppins fonts

---
+++ client/src/util/fonts.ts
@@ -0,0 +1,11 @@
+const Fonts = {
+    PoppinsBlack: 'Poppins-Black',
+    PoppinsBold: 'Poppins-Bold',
+    PoppinsItalic: 'Poppins-Italic',
+    PoppinsLight: 'Poppins-Light',
+    PoppinsMedium: 'Poppins-Medium',
+    PoppinsRegular: 'Poppins-Regular',
+    PoppinsSemiBold: 'Poppins-SemiBold',
+};
+
+export default Fonts;
